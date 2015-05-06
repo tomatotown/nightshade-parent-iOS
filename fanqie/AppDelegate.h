146903964284f124aa67d5abfb58a59eec4b5407
李俊 <Lijun@lijundeMBP.lan>
2015-05-06 17:21:52 +0800

    Initial Commit

---
+++ fanqie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fanqie
+//
+//  Created by 李俊 on 15/5/6.
+//  Copyright (c) 2015年 lijun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
